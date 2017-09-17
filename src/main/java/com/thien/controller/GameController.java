77833d1d0de840effb9c7ac8039f61e81350cfb2
Thien <thien.udomsrirungruang@gmail.com>
2017-09-17 16:45:38 +0700

    Added a mapping for the random picture generator

--- src/main/java/com/thien/controller/GameController.java
+++ src/main/java/com/thien/controller/GameController.java
@@ -1,12 +1,28 @@
 package com.thien.controller;
 
+import com.thien.entity.PictureInfo;
+import com.thien.service.PictureGetter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GameController {
+
+    @Autowired
+    private PictureGetter pictureGetter;
+
     @RequestMapping("")
     public String getMainPage(){
         return "main";
     }
+
+    @RequestMapping(value = "/random-picture", method = RequestMethod.POST)
+    @ResponseBody
+    public PictureInfo getRandomPicture(){
+        return pictureGetter.getRandomPicture();
+    }
+
 }
