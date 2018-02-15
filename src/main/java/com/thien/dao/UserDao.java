da192ab1eea88ac0bec404c13a0d83b3295d6bdb
Thien <thien.udomsrirungruang@gmail.com>
2018-02-15 17:29:12 +0700

    Sign up now adds "user" user-role in the table so they can access main page

--- src/main/java/com/thien/dao/UserDao.java
+++ src/main/java/com/thien/dao/UserDao.java
@@ -1,13 +1,13 @@
 package com.thien.dao;
 
-import com.thien.entity.PictureInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -15,6 +15,8 @@ import java.util.List;
 public class UserDao {
     private static final String GET_RANDOM_PICTURE_SQL = "select id from users where username=?";
     private static final String ENTER_NEW_USER_SQL = "insert into users (username, pwd, enabled) values (?, ?, 1)";
+    private static final String GET_USER_ID_SQL = "select id from users where username = ?";
+    private static final String SET_USER_ROLE_SQL = "insert into user_roles (user_id, role) values (?, ?)";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -29,7 +31,22 @@ public class UserDao {
         return rs.get(0);
     }
 
-    public void enterNewUser(String username, String encodedPassword){
+    public void enterNewUser(String username, String encodedPassword) {
         jdbcTemplate.update(ENTER_NEW_USER_SQL, username, encodedPassword);
+        try {
+            jdbcTemplate.update(SET_USER_ROLE_SQL, getIdByUsername(username), "user");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getUserIdByUsername(String username){
+        List<Integer> rs = jdbcTemplate.query(GET_USER_ID_SQL, new RowMapper<Integer>(){
+            @Override
+            public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return rs.getInt(1);
+            }
+        }, username);
+        return rs.get(0);
     }
 }
